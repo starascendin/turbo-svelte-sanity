ad3cfc33b52c5f150a3c3def0f6fd239ab9b6bb6
King Diamond Ape <star.ascendin@gmail.com>
2023-05-25 15:20:07 -0400

    WIP

--- apps/site/src/routes/+page.ts
+++ apps/site/src/routes/+page.ts
@@ -1,7 +1,17 @@
 import type { PageLoad } from './$types';
+import sanityClient from "@sanity/client";
+
+const client = sanityClient({
+	projectId: "9s9ectym",
+	dataset: "production",
+	apiVersion: "2022-09-09",
+	useCdn: false
+  });
+  
 
 export const load: PageLoad = async (event) => {
-
+	const data = await client.fetch(`*[_type == "pet"]`);
+	
 	return {
 		cms: [{title: 'testing', body: "testing body"}, {title: 'testing2', body: "testing body2"}],
 	};
