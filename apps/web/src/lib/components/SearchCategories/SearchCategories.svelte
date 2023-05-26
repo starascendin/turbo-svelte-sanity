1de69e7fdcb5c9a6f7c69912246f20847845b35e
King Diamond Ape <star.ascendin@gmail.com>
2023-05-26 15:25:37 -0400

    update

--- apps/web/src/lib/components/SearchCategories/SearchCategories.svelte
+++ apps/web/src/lib/components/SearchCategories/SearchCategories.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import { Checkbox } from 'flowbite-svelte';
 
-	export let blogs: any[];
+	// export let blogs: any[];
 
-	export let categories: any[];
+	// export let categories: any[];
 
-	export let selectedCategories: any[];
-	export let filteredBlogs: any[];
+	// export let selectedCategories: any[];
+	// export let filteredBlogs: any[];
 </script>
 
 <div class="container">
 	<div class="categories">
-		{#each categories as category (category)}
+		<!-- {#each categories as category (category)}
 			<label for="">
 				<Checkbox on:change={(e) => handleCheckboxChange(category, e)} />
 				{category}
 			</label>
-		{/each}
+		{/each} -->
 	</div>
 </div>
